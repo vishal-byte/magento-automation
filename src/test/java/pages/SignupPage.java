38c50589d101d936f673a1ba29a30cdc95f05ffd
unknown <vishalchauhan1072@gmail.com>
2025-06-25 22:39:56 +0530

    Add SignupPage.java for registration page actions

--- src/test/java/pages/SignupPage.java
+++ src/test/java/pages/SignupPage.java
@@ -1,4 +1,44 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class SignupPage {
+
+    WebDriver driver;
+
+    // === Locators for Sign-up fields ===
+    private By firstName = By.id("firstname");
+    private By lastName = By.id("lastname");
+    private By email = By.id("email_address");
+    private By password = By.id("password");
+    private By confirmPassword = By.id("password-confirmation");
+    private By createAccountButton = By.cssSelector("button[title='Create an Account']");
+
+    // === Constructor ===
+    public SignupPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // === Actions ===
+    public void enterFirstName(String fname) {
+        driver.findElement(firstName).sendKeys(fname);
+    }
+
+    public void enterLastName(String lname) {
+        driver.findElement(lastName).sendKeys(lname);
+    }
+
+    public void enterEmail(String emailId) {
+        driver.findElement(email).sendKeys(emailId);
+    }
+
+    public void enterPassword(String pwd) {
+        driver.findElement(password).sendKeys(pwd);
+        driver.findElement(confirmPassword).sendKeys(pwd);
+    }
+
+    public void clickCreateAccount() {
+        driver.findElement(createAccountButton).click();
+    }
 }
